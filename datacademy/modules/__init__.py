74b954e353477d5153a0a776ada80d5eb021814b
Paulo Mota <p.mota@datacation.nl>
2024-01-02 13:40:53 +0100

    module 10 added on init file

--- datacademy/modules/__init__.py
+++ datacademy/modules/__init__.py
@@ -5,4 +5,4 @@ from .m03_sql import Module03
 from .m04_ml import Module04
 from .m05_api import Module05
 
-__all__ = ['Module02', 'Module03', 'Module04', 'Module05']
+__all__ = ['Module02', 'Module03', 'Module04', 'Module05','Module10']

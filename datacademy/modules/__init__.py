2581621fedda889fa88c67609501dbe068b7ddd2
Remco Coppens <remcocoppens@outlook.com>
2024-02-20 11:14:25 +0100

    Added M07

--- datacademy/modules/__init__.py
+++ datacademy/modules/__init__.py
@@ -4,6 +4,7 @@ from .m02_pcc import Module02
 from .m03_sql import Module03
 from .m04_ml import Module04
 from .m05_api import Module05
+from .m07_mla import Module07
 from .m10_final import Module10
 
-__all__ = ['Module02', 'Module03', 'Module04', 'Module05','Module10']
+__all__ = ['Module02', 'Module03', 'Module04', 'Module05', 'Module07', 'Module10']
